56fdca2e4632f96921f3b870e02b1f8bcb5ec688
tsquaredhere <tsquaredank@gmail.com>
2020-01-12 17:30:37 -0800

    input bar

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
+  //gives array of 2 elements, first is the ^^ initial default of your state, and then the second is the method you will call to change it. 
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 70}}>
+
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems:'center'}}>
+        <TextInput placeholder="Goals" 
+        style={{ width: '80%', borderColor: 'black', padding: 10, borderBottomWidth: 1}}
+        />
+        <Button title="ADD" />
+      </View>
+
+      <View>
+
+      </View>
+
     </View>
   );
 }
