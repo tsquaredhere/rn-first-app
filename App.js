6b3ac1863067bc455815d3406a3c4a3fefbd6f13
tsquaredhere <tsquaredank@gmail.com>
2020-01-13 18:11:15 -0800

    modal

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, View, Button, TextInput, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem'
 import GoalInput from './components/GoalInput'
 
@@ -7,12 +7,18 @@ export default function App() {
   
   //gives array of 2 elements, first is the ^^ initial default of your state, and then the second is the method you will call to change it. 
   const [myGoals, setMyGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   
   const addGoalHandler = (goalName) => {
     setMyGoals([...myGoals, { key: Math.random().toString(), value: goalName }]);
     //setmyGoals(myGoals => [..myGoals, { key: Math.random().toString(), value: goalName }); <- better because gives you guaranteed state snapshot.
     //FlatList wants objects with a key
+    setIsAddMode(false);
+  }
+
+  const cancelGoalAddHandler = () => {
+    setIsAddMode(false);
   }
 
   const deleteGoalHandler = goalId => {
@@ -23,12 +29,14 @@ export default function App() {
 
   return (
     <View style={styles.screen}>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} /> 
 
-      <GoalInput addGoal={addGoalHandler} />
+      <GoalInput addGoal={addGoalHandler} onCancel={cancelGoalAddHandler} visible={isAddMode}/>
 
       <FlatList 
         data={myGoals} 
-        renderItem={itemData => <GoalItem id={itemData.item.key} title={itemData.item.value} onDelete={deleteGoalHandler}/>} />
+        renderItem={itemData => <GoalItem id={itemData.item.key} title={itemData.item.value} onDelete={deleteGoalHandler}/>} 
+      />
     </View>
   );
 }
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,37 +1,65 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Modal} from 'react-native';
 
 const GoalInput = props => {
     const [enteredGoal, setEnteredGoal] = useState('');
-    const goalInputHandler = (enteredText) => {
-        setEnteredGoal(enteredText);
-      }
-    return (
-        <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Goals"
-          style={styles.textInput}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
 
-        <Button title="ADD" onPress={props.addGoal.bind(this, enteredGoal)} />
-      </View>
+    const goalInputHandler = (enteredText) => {
+      setEnteredGoal(enteredText);
+    }
+
+    const addGoalHander = () => {
+      props.addGoal(enteredGoal);
+      setEnteredGoal('');
+    }
+    return (
+      <Modal visible={props.visible} animationType="slide">
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Goals"
+            style={styles.textInput}
+            onChangeText={goalInputHandler}
+            value={enteredGoal}
+          />
+          <View style={styles.buttonContainer}> 
+            <View style={styles.button} >
+              <Button title="CANCEL" color="red" onPress={props.onCancel}/>
+            </View>
+            <View style={styles.button} >
+              <Button title="ADD" onPress={addGoalHander} />
+            </View>
+          </View>
+        </View>
+      </Modal>
     )
 }
 
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-      },
-      textInput: {
-        width: '80%',
-        borderColor: 'black',
-        padding: 10,
-        borderBottomWidth: 1
-      }
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    textInput: {
+      width: '80%',
+      borderColor: 'black',
+      padding: 10,
+      marginBottom: 20,
+      borderBottomWidth: 1
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '60%'
+    },
+    button: {
+      width: '45%'
+    }
 })
+
+//view does not take up full width of the modal. so it only takes up the space of the children (Textinput and button)
+// to make the view take up the whole space, set flex to 1, if it is the only item, it will take up the whole space. 
+// you have to set flex to something. (anything if its the only child)
 export default GoalInput
